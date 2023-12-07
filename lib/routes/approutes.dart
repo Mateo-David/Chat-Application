60871ece81de6d902943e7bd58bf094fc4829fe1
Mateo-David <muneeb.shahid@launchbox.pk>
2023-12-07 19:35:47 +0500

    first commit

---
+++ lib/routes/approutes.dart
@@ -0,0 +1,9 @@
+// import 'package:get/get.dart';
+
+
+// class AppRoutes {
+//   static appRoutes() => [
+//         GetPage(
+//             name: RoutesName.GoogleMapScreen, page: () => GoogleMapScreen()),
+//       ];
+// }
